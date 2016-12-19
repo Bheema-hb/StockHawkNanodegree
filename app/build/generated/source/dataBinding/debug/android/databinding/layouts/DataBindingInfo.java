e4048b74486eb589b6600da2b1bac5d677942595
Bheema <bheemaray@techjini.com>
2016-12-19 18:27:54 +0530

    Stock hawk initial commit

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="b8755a48-7480-4a4f-bf8a-2c20b943c1fa", modulePackage="com.udacity.stockhawk", sdkRoot="/Users/Bheema/Library/Android/sdk",layoutInfoDir="/Users/Bheema/Downloads/StockHawk-Divya/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=21,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
