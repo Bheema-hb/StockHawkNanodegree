b03b75d9dacb7f987e3cea050a49fb30892c0fec
Bheema <bheemaray@techjini.com>
2016-12-20 11:12:10 +0530

    build cleaup

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="b8755a48-7480-4a4f-bf8a-2c20b943c1fa", modulePackage="com.udacity.stockhawk", sdkRoot="/Users/Bheema/Library/Android/sdk",layoutInfoDir="/Users/Bheema/Downloads/StockHawk-Divya/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=21,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="16c9a721-5af2-4fb7-8fb7-6937fb96b188", modulePackage="com.udacity.stockhawk", sdkRoot="/Users/Bheema/Library/Android/sdk",layoutInfoDir="/Users/Bheema/Android-Projects/NanoDegree/StockHawk-Bheema/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=21,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
